1c4ea70c0bc915725c6f26a4cbcb0d67a8ac9087
Râmerosn Rodrigues <ramersonrodrigues.ti@gmail.com>
2026-08-15 12:18:27 -0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  turbopack: {},
   webpack: (config, { dev }) => {
     if (dev) {
       config.watchOptions = {
